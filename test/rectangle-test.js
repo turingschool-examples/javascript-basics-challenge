d6d3bf0ef090784fd5e1818a5b64311943deb36c
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-05-01 13:45:54 -0600

    Add finding diagonal function

--- test/rectangle-test.js
+++ test/rectangle-test.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const {area, perimeter} = require('../lib/rectangle');
+const {area, perimeter, diagonal} = require('../lib/rectangle');
 
 describe('area calculation', function() {
   it('calculates area of a rectangle', function() {
@@ -15,6 +15,15 @@ describe('perimeter calculation', function() {
     var rectangle = { width: 10, height: 20 };
     var expected = 60;
     var result = perimeter(rectangle);
+    assert.equal(result, expected);
+  });
+});
+
+describe('diagonal length calculation', function() {
+  it('calculates diagonal length of a rectangle', function() {
+    var rectangle = { width: 3, height: 4 };
+    var expected = 5;
+    var result = diagonal(rectangle);
     assert.equal(result, expected);
   });
 });
