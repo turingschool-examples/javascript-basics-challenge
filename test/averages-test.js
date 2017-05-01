4ebb8286a37aa2b67da467f848893730ca405bbd
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-05-01 14:42:17 -0600

    Add median function

--- test/averages-test.js
+++ test/averages-test.js
@@ -1,9 +1,18 @@
 const assert = require('chai').assert;
-const total = require('../lib/averages');
+const {total, median} = require('../lib/averages');
 
 describe('determine total', function() {
   it('determines sum of all elements in an array', function() {
     var numbers = [1, 2, 3, 4, 5];
     assert.equal(total(numbers), 15);
+  });
+});
+
+describe('determine median', function() {
+  it('determines the median value for elements in an array', function() {
+    var numbers = [1, 2, 3, 4, 5];
+    var numbersEven = [1, 2, 3, 4, 5, 6];
+    assert.equal(median(numbers), 3);
+    assert.equal(median(numbersEven), 3.5);
   });
 });
