e55ec9eeec0290dd4cc774a0c4551309c7e4d3df
Charlotte Moore <mooc123@gmail.com>
2017-05-01 13:40:42 -0600

    rectangle area complete

--- test/rectangle_test.js
+++ test/rectangle_test.js
@@ -1,9 +1,11 @@
 const assert = require('chai').assert
-const insertionSort = require('../lib/rectangle')
+const rectangle = require('../lib/rectangle')
+var pry = require('pryjs');
 
 describe('rectangles', function() {
  it("can return the area", function () {
-   var rectangle = {width: 10, height: 20}
-   assert.equal(area(rectangle), 200)
+  //  eval(pry.it);
+   var actualRectangle = {width: 10, height: 20}
+   assert.equal(rectangle.area(actualRectangle), 200)
  });
 });
