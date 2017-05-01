bc972610ac6bf7eb50b483ad96905357ebd0ea0d
Charlotte Moore <mooc123@gmail.com>
2017-05-01 14:04:46 -0600

    rectangles complete

--- test/rectangle_test.js
+++ test/rectangle_test.js
@@ -3,9 +3,27 @@ const rectangle = require('../lib/rectangle')
 var pry = require('pryjs');
 
 describe('rectangles', function() {
- it("can return the area", function () {
-  //  eval(pry.it);
-   var actualRectangle = {width: 10, height: 20}
-   assert.equal(rectangle.area(actualRectangle), 200)
- });
+   it("can return the area", function () {
+     var actualRectangle = {width: 10, height: 20}
+     assert.equal(rectangle.area(actualRectangle), 200)
+   });
+
+   it("can return the perimeter", function(){
+     var actualRectangle = {width: 10, height: 20}
+     assert.equal(rectangle.perimeter(actualRectangle), 60)
+   });
+
+   it ("can return the diagonal", function(){
+    var actualRectangle = {width: 10, height: 20}
+    assert.equal(rectangle.diagonal(actualRectangle), 22.36)
+   });
+
+   it("can tell if the rectangle is a square", function(){
+     var actualRectangle = {width: 10, height: 20}
+    
+     assert.equal(rectangle.isSquare(actualRectangle), false)
+     var square = {width: 10, height: 10}
+     assert.equal(rectangle.isSquare(square), true)
+   })
+
 });
