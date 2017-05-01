4e3e44b0d0e6601b5fd848dd5fb2184021e408f2
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-05-01 13:40:48 -0600

    Add perimeter function

--- test/rectangle-test.js
+++ test/rectangle-test.js
@@ -1,11 +1,20 @@
 const assert = require('chai').assert;
-const area = require('../lib/rectangle');
+const {area, perimeter} = require('../lib/rectangle');
 
 describe('area calculation', function() {
   it('calculates area of a rectangle', function() {
     var rectangle = { width: 10, height: 20 };
     var expected = 200;
     var result = area(rectangle);
+    assert.equal(result, expected);
+  });
+});
+
+describe('perimeter calculation', function() {
+  it('calculates perimeter of a rectangle', function() {
+    var rectangle = { width: 10, height: 20 };
+    var expected = 60;
+    var result = perimeter(rectangle);
     assert.equal(result, expected);
   });
 });
