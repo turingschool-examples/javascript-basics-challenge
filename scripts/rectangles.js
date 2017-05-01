1973f27ef631b72cc37a5f4afcc18aa008462fc1
Edilene <edilene_cruz@yahoo.com.br>
2017-05-01 13:57:32 -0600

    add rectangle area functionality and test

---
+++ scripts/rectangles.js
@@ -0,0 +1,24 @@
+var pry = require('pryjs');
+
+function area(rectangle) {
+  return rectangle.width * rectangle.height
+};
+
+function perimeter(rectangle) {
+  return (rectangle.width*2) + (rectangle.height*2)
+}
+
+function diagonal(rectangle){
+  var answer = Math.sqrt(Math.pow(rectangle.width, 2) + Math.pow(rectangle.height, 2))
+  return answer
+}
+
+function isSquare(rectangle){
+  if (rectangle.width == rectangle.height) {
+    return true
+  } else {
+    return false
+  }
+}
+
+module.exports = {area, perimeter, diagonal, isSquare}
---
+++ tests/rectangles-test.js
@@ -0,0 +1,37 @@
+const assert = require('chai').assert;
+const rectangle = require('../scripts/rectangles');
+var pry = require('pryjs');
+
+describe('rectangle', function() {
+  context('area', function(){
+    it('it can calculate area for a rectangle', function() {
+      var r = {width: 30, height: 13}
+
+      assert.equal(rectangle.area(r), 390)
+    });
+  });
+
+  context('perimeter', function(){
+    it('returns the perimeter of the rectangle', function() {
+      var r = {width: 20, height: 10}
+
+      assert.equal(rectangle.perimeter(r), 60)
+    });
+  });
+
+  context('diagonal', function(){
+    it('returns the diagonal of the rectangle', function() {
+      var r = {width: 20, height: 10}
+
+      assert.equal(rectangle.diagonal(r), 22.360679774997898)
+    });
+  });
+
+  context('isSquare', function() {
+    it('retunrs true if the rectangle is a square', function() {
+      var r = {width: 20, height: 20}
+
+      assert.isTrue(rectangle.isSquare(r), true)
+    });
+  });
+});
