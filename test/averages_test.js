d3cf60672a4489f7463dfb16bc81d16a347e249c
blackknight75 <devbox4714@gmail.com>
2017-05-03 14:08:13 -0600

    mode working

--- test/averages_test.js
+++ test/averages_test.js
@@ -2,6 +2,7 @@ var assert = require('chai').assert
 var total = require('../lib/averages').total
 var meanAverage = require('../lib/averages').meanAverage
 var medianAverage = require('../lib/averages').medianAverage
+var modeAverage = require('../lib/averages').modeAverage
 
 describe('Averages', function(){
   it('can total collection', function(){
@@ -18,5 +19,17 @@ describe('Averages', function(){
 
   it('returns median average of unsorted collection', function(){
     assert.equal(medianAverage([12,2,30,4,5]), 5)
+  })
+
+  it('returns mode average of collection', function(){
+    assert.equal(modeAverage([1,1,4,3,2]), 1)
+  })
+
+  it('returns mode average of large unorganized collection', function(){
+    assert.equal(modeAverage([500,1,5,1,4,35,1,3,7,3,2,5,1,2,1000,30,1,]), 1)
+  })
+
+  it('returns mode average of large unorganized collection again', function(){
+    assert.equal(modeAverage([500,8,5,8,4,35,8,3,7,3,2,5,8,2,8000,30,8,]), 8)
   })
 })
