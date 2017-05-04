35b333ea3528dc401ad7d800edc34d74d5451dd0
Spencer Carter <scmountain@gmail.com>
2017-05-03 22:01:10 -0600

    median finished

--- test/average_test.js
+++ test/average_test.js
@@ -2,6 +2,7 @@ var assert = require('chai').assert
 var total = require('../lib/average').total
 var mean = require('../lib/average').mean
 var median = require('../lib/average').median
+var mode = require('../lib/average').mode
 
 describe ('Average', function(){
   it('total value', function(){
@@ -14,9 +15,18 @@ describe ('Average', function(){
     assert.equal(mean(list), 3.5)
   })
 
-  it('average', function(){
+  it('median', function(){
     var list = [1,2,3,4,5,6,7]
     assert.equal(median(list), 4)
   })
 
+  it('median even', function(){
+    var list = [1,2,3,4,5,6]
+    assert.equal(median(list), 3.5)
+  })
+
+  it('mode', function(){
+    var list = [1,1,3,4,5,6,1]
+    assert.equal(mode(list), 1)
+  })
 })
