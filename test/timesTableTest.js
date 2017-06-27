fa4353914bc5b844ae664b3f49255792d70c390c
Valerie Trudell <v.trudell@protonmail.com>
2017-06-27 15:00:20 -0600

    Adds times tables

--- test/timesTableTest.js
+++ test/timesTableTest.js
@@ -2,14 +2,9 @@ const timesTable  = require('../lib/timesTable')
 const assert = require('chai').assert
 
 describe("timesTable", () => {
-  xit("regular", () => {
+  it("regular", () => {
     let assertion = timesTable(5)
-    let expected  = `1  2  3  4  5
-    2  4  6  8  10
-    3  6  9  12 15
-    4  8  12 16 20
-    5  10 15 20 25`
-
+    let expected  = '1 2 3 4 5 \n2 4 6 8 10 \n3 6 9 12 15 \n4 8 12 16 20 \n5 10 15 20 25 \n'
     assert.equal(assertion, expected)
   })
 
