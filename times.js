1329e85fa60331a0c8b864a37682e1af2c35bba1
jk1dd <portajonk@gmail.com>
2017-06-26 18:13:45 -0600

    times table add printing table and fix test

--- times.js
+++ times.js
@@ -4,7 +4,7 @@ function timesTable(n) {
   var row1 = []
   var all = []
 
-  for(var i = 1; i < n; i++) {
+  for(var i = 1; i <= n; i++) {
     row1.push(i)
   }
   all.push(row1)
@@ -15,6 +15,13 @@ function timesTable(n) {
     })
     all.push(row2)
   }
+
+  var table = ""
+  for(var i=0; i<all.length; i++) {
+    table += all[i].join(' ') + '\n'
+  }
+  // eval(pry.it)
+  return table;
   // for(var i = 0; i < all[i].length; i++) {
   //   for(var z = 0; z < all.length; z++) {
   //     console.log(all[z][i])
