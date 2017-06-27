808043539fb2ca6226958136e54b841ec3df8735
bschwartz10 <bschwartz92@gmail.com>
2017-06-26 21:51:09 -0600

    rectangle functions
    perimeter function
    diagonal function
    isSquare function

---
+++ rectangles.js
@@ -0,0 +1,28 @@
+function area(rectangle) {
+  return rectangle.width * rectangle.height;
+}
+
+function perimeter(rectangle) {
+  return (rectangle.width * 2) + (rectangle.height * 2);
+}
+
+function diagonal(rectangle) {
+  var diagonalLength = Math.pow(rectangle.width, 2) + Math.pow(rectangle.height, 2);
+  return Math.sqrt(diagonalLength);
+}
+
+function isSquare(rectangle) {
+  if(rectangle.width === rectangle.height) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+module.exports = {
+  area,
+  perimeter,
+  diagonal,
+  isSquare
+}
