7e959f239db8b65fc93ff6f8e715a1c60def1be7
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-09 16:40:26 -0600

    completes rectangles challenges

---
+++ test/rectangles-test.js
@@ -0,0 +1,56 @@
+const assert = require('chai').assert
+
+const rectangle = require('../lib/rectangles');
+const area = rectangle.area;
+const perimeter = rectangle.perimeter;
+const diagonal = rectangle.diagonal
+const isSquare = rectangle.square
+
+
+describe("area", function() {
+  context("with valid inputs", function() {
+    it("outputs the calculated area of that rectangle", function() {
+      var rectangle = {width: 10, height: 20}
+
+      assert.equal(area(rectangle), 200)
+    })
+  })
+})
+
+describe("perimeter", function() {
+  context("with valid inputs", function() {
+    it("outputs the calculated perimeter of a rectangle", function() {
+      var rectangle = {width: 10, height: 20}
+
+      assert.equal(perimeter(rectangle), 60)
+    })
+  })
+})
+
+describe("diagonal", function() {
+  context("with valid inputs", function() {
+    it("outputs the length of the rectangle's diagonal", function() {
+      var rectangle = {width: 10, height: 20}
+
+      assert.equal(diagonal(rectangle), 22.36)      
+    })
+  })
+})
+
+describe("isSquare", function() {
+  context("when inputed a square", function() {
+    it("outputs true", function() {
+      var rectangle = {width: 10, height: 10}
+      
+      assert.isTrue(isSquare(rectangle))
+    })
+  })
+  context("when inputed a non-square", function() {
+    it("outputs false", function() {
+      var rectangle = {width: 20, height: 10}
+      
+      assert.isFalse(isSquare(rectangle))
+    })
+  })
+})
+
