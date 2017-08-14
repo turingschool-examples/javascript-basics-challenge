39f8949e2b2939e0460e75c550893079b5f045e5
squeemishly <squeemishly@gmail.com>
2017-08-14 15:29:32 -0600

    it finds the area of a rectangle

--- rectangles/area.js
+++ rectangles/area.js
@@ -1,5 +1,7 @@
-var area = function(width, height) {
+var pry = require('pryjs');
 
+var area = function(rectangle) {
+  return rectangle["width"] * rectangle["height"];
 }
 
 module.exports = area;
--- rectangles/test/rectangles-tests.js
+++ rectangles/test/rectangles-tests.js
@@ -1,9 +1,14 @@
 var assert = require('chai').assert
 var area = require('../area')
 
-describe("it can measure a rectangle", function() {
-  it(", specifically, the area", function() {
-    var rectangle = {width: 10, height: 20}
-    assert.equal(area(rectangle), 200)
+describe("it can measure a rectangle, specifically: ", function() {
+  it("the area", function() {
+    var rectangle = {width: 10, height: 20};
+    assert.equal(area(rectangle), 200);
+  })
+
+  it("the perimeter", function() {
+    var rectangle = {width: 10, height: 20};
+    assert.equal(perimiter(rectangle), 60);
   })
 })
