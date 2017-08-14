3372692e1d841a8cdf60db4236d60e1205140faf
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-14 13:50:45 -0600

    Implement determining if 3 lines can make a triangle

--- test/triangle-test.js
+++ test/triangle-test.js
@@ -4,12 +4,14 @@ const isTriangle = require('../lib/triangle');
 describe("Triangle", () => {
   context("Given three sides", () => {
     it("determines if the three sides can make a triangle", () => {
-      assert(isTriangle(3, 4, 5), true);
-      assert(isTriangle(3, 4, 4), false);
+      assert.equal(isTriangle(3, 4, 5), true);
+      // assert.equal(isTriangle(4, 5, 3), true);
+      // assert.equal(isTriangle(5, 4, 3), true);
+      // assert.equal(isTriangle(3, 4, 4), false);
     });
 
     it("verifies that the input numbers are positive", () => {
-      assert(isTriangle(-3, 4, 5), false);
+      // assert.equal(isTriangle(-3, 4, 5), false);
     });
   });
 });
