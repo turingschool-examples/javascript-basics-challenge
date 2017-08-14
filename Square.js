5502ea10b37637c89b1b4efcf42b08266a70b5a4
Chris Smith <42@chrisryansmith.com>
2017-08-14 11:48:14 -0600

    can calculate the perimeter of the square

--- Square.js
+++ Square.js
@@ -4,7 +4,13 @@ class Square {
     this.height = dimensions.height
   }
 
+  area() {
+    return this.width * this.height
+  }
 
+  perimeter() {
+    return this.width * 2 + this.height * 2
+  }
 }
 
 module.exports = Square
--- test/square-test.js
+++ test/square-test.js
@@ -7,13 +7,28 @@ describe('Square', () => {
       const mySquare = new square({width: 10, height: 20})
       assert.instanceOf(mySquare, square);
     })
+
     it('has a width', () => {
       const mySquare = new square({width: 10, height: 20})
       assert.equal(mySquare.width, 10)
     })
+
     it('has a height', () => {
       const mySquare = new square({width: 10, height: 20})
       assert.equal(mySquare.height, 20)
+    })
+  })
+  context ('.area()', () => {
+    it('it can return the area of the rectangle', () => {
+      const mySquare = new square({width: 10, height: 20})
+      assert.equal(mySquare.area(), 200)
+    })
+  })
+
+  context ('.perimeter()', () => {
+    it('it can return the perimeter of the rectangle', () => {
+      const mySquare = new square({width: 10, height: 20})
+      assert.equal(mySquare.perimeter(), 60)
     })
   })
 })
