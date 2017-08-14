d90d77dc875a72edeed84aee382e979ec084daf8
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-08-14 14:59:52 -0600

    Create and test #isTriangle, #noZeros, and #noNegatives

--- test/triangle-test.js
+++ test/triangle-test.js
@@ -1,7 +1,33 @@
 const assert = require('chai').assert;
-const isTriangle = require('../triangle').isTriangle
+const isTriangle = require('../triangle').isTriangle;
+const noZeros = require('../triangle').noZeros;
+const noNegatives = require('../triangle').noNegatives;
 
-describe("isTriangle", function(){
+describe("triangle funtions", function(){
+  it("returns false if any of the numbers are zero", function(){
+    var expected = false;
+    var actual = noZeros(6, 5, 0);
+    assert.equal(expected, actual);
+  });
+
+  it("returns true if none of the numbers are zero", function(){
+    var expected = true;
+    var actual = noZeros(6, 5, 4);
+    assert.equal(expected, actual);
+  });
+
+  it("returns false if any of the numbers are negative", function(){
+    var expected = false;
+    var actual = noNegatives(6, 5, -2);
+    assert.equal(expected, actual);
+  });
+
+  it("returns true if none of the numbers are negative", function(){
+    var expected = true;
+    var actual = noNegatives(6, 5, 4);
+    assert.equal(expected, actual);
+  });
+
   it("returns true if the three numbers form an equilateral triangle", function(){
     var expected = true;
     var actual = isTriangle(4, 4, 4);
@@ -32,16 +58,3 @@ describe("isTriangle", function(){
     assert.equal(expected, actual);
   });
 });
-
-
-
-// isTriangle(0,0,0) # => false, because a triangle can't have 0 length sides
-// isTriangle(-2,1,5) # => false, because a triangle can't have negative length sides
-
-// isTriangle(4,4,4) # => true, an equilateral triangle
-
-// isTriangle(6,8,10) # => true, a right triangle
-// isTriangle(8,6,10) # => true, the same right triangle
-// isTriangle(10,8,6) # => true, the same right triangle
-
-// isTriangle(4, 8, 50) #=> false, no such triangle exists
