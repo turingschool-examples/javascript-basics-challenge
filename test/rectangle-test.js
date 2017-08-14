725a761cebad4f8bf9b3314b255d379745bd507e
Aaron Olsen <mraaronolsen@gmail.com>
2017-08-14 13:54:50 -0600

    Rectangle

--- test/rectangle-test.js
+++ test/rectangle-test.js
@@ -1,32 +1,33 @@
-const assert = require('chai').assert;
-const rectangle = require('../lib/rectangle');
+const shape = require('../lib/shape');
 
 describe('Rectangle', function(){
   it('Is a Rectangle Object', function(){
-    rectangle = new Rectangle(2, 4);
-    assert.equal(rectangle.width, 2);
-    assert.equal(rectangle.height, 4);
+    var rectangle = new shape.Rectangle(2, 4);
+
+    assert.equal(2, rectangle.width);
+    assert.equal(4, rectangle.height);
   });
 
   it('Calculates Area', function(){
-    rectangle = new Rectangle(2, 4);
-    assert.equal(rectangle.area, 8)
+    rectangle = new shape.Rectangle(2, 4);
+
+    assert.equal(8, rectangle.area())
   });
 
   it('Calculates Perimeter', function(){
-    rectangle = new Rectangle(2, 4);
-    assert.equal(rectangle.perimeter, 12)
-  })
+    rectangle = new shape.Rectangle(2, 4);
+    assert.equal(12, rectangle.perimeter())
+  });
 
   it('Calculates Diagonal', function(){
-    rectangle = new Rectangle(3, 4);
-    assert.equal(rectangle.diagonal, 5)
+    rectangle = new shape.Rectangle(3, 4);
+    assert.equal(5, rectangle.diagonal())
   });
 
   it('Knows if Square or not', function(){
-    rectangle = new Rectangle(3, 4);
-    square = new Rectangle(4, 4);
-    assert.isFalse(rectangle.isSquare);
-    assert.isTrue(rectangle.isSquare);
+    rectangle = new shape.Rectangle(3, 4);
+    square = new shape.Rectangle(4, 4);
+    assert.isFalse(rectangle.isSquare());
+    assert.isTrue(square.isSquare());
   });
-}
+});
