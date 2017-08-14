cdeed8fe8a99e367f88965a99181bc5784453566
Chris Smith <42@chrisryansmith.com>
2017-08-14 14:40:29 -0600

    add mode object return

--- test/averages-test.js
+++ test/averages-test.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert
-const {total, mean} = require('../averages.js')
+const {total, mean, median, mode} = require('../averages.js')
 
 describe('total()', () => {
   it('it sums an array', () => {
@@ -16,9 +16,31 @@ describe('mean()', () => {
   })
 })
 
+describe('median()', () => {
+  it('returns middle number of an array with odd length', () => {
+    assert.equal(median([1,1,1]), 1)
+    assert.equal(median([3,1,2]), 2)
+    assert.equal(median([3,1,200, 2, 100]), 3)
+  })
+  it('returns middle number of an array with even length', () => {
+    assert.equal(median([1,1,1, 2]), 1)
+    assert.equal(median([3,1,2, 4]), 2.5)
+    assert.equal(median([3,1,200, 5]), 4)
+  })
+})
 
-// 1. `total` - returns the total of all the values in the array
-// 2. `mean` - returns the mean (average) of the array
-// 3. `median` - returns the median of the array
-// 4. `mode`- returns an object representing the mode(s) of the array
-//     with the property being the mode and the value being the frequency
+describe('mode()', () => {
+  it('returns object', () => {
+    assert.instanceOf(mode([1]), Object)
+  })
+
+  it('the property is the mode', () => {
+    result = mode([1,2,3,2])
+    assert.equal(Object.keys(result)[0], 2)
+  })
+
+  it('the value is the frequency', () => {
+    result = mode([1,2,3,2, 2])
+    assert.equal(result[2], 3)
+  })
+})
