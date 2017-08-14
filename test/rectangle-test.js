6e5674f55db6fdd6345f1d2a4c7b4b53ab00af50
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-14 12:05:07 -0600

    Add test for calculating rectangle diagonal

--- test/rectangle-test.js
+++ test/rectangle-test.js
@@ -4,14 +4,19 @@ const perimeter = require('../lib/rectangle').perimeter;
 
 describe("Rectangle", () => {
   context("Given a width and a height", () => {
+    var rectangle = { width: 10, height: 20 };
+
     it("calculates the area", () => {
-      var rectangle = { width: 10, height: 20 };
       assert.equal(area(rectangle), 200);
     });
 
     it("calculates the perimeter", () => {
-      var rectangle = { width: 10, height: 20 };
       assert.equal(perimeter(rectangle), 60);
+    });
+
+    it("calculates the diagonal", () => {
+      var rectangle_3_4 = { width: 3, height: 4 }
+      assert.equal(diagonal(rectangle), 5)
     });
   });
 });
