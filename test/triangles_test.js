830bb60918c6b29bbc514f0a26172e4519a9352c
Padraic O'Neill <pconeill@gmail.com [core]>
2017-08-14 15:07:10 -0600

    adds right triangle test and functions

--- test/triangles_test.js
+++ test/triangles_test.js
@@ -1,5 +1,4 @@
-// isTriangle(0,0,0) # => false, because a triangle can't have 0 length sides
-// isTriangle(-2,1,5) # => false, because a triangle can't have negative length sides
+
 // isTriangle(6,8,10) # => true, a right triangle
 // isTriangle(8,6,10) # => true, the same right triangle
 // isTriangle(10,8,6) # => true, the same right triangle
@@ -16,6 +15,45 @@ describe('With a suite of geometric functions', function() {
     var x = 3
     var y = 3
     var z = 3
+
+    triangle = new Triangle(x, y, z);
+
+    var actual = triangle.isTriangle()
+    var expected = true
+
+    assert.equal(actual, expected);
+  });
+
+  it('returns false if any side is zero length', function() {
+    var x = 0
+    var y = 3
+    var z = 3
+
+    triangle = new Triangle(x, y, z);
+
+    var actual = triangle.isTriangle()
+    var expected = false
+
+    assert.equal(actual, expected);
+  });
+
+  it('returns false if any side is negative length', function() {
+    var x = 3
+    var y = 3
+    var z = -3
+
+    triangle = new Triangle(x, y, z);
+
+    var actual = triangle.isTriangle()
+    var expected = false
+
+    assert.equal(actual, expected);
+  });
+
+  it('can determine a right triangle', function() {
+    var x = 3
+    var y = 4
+    var z = 5
 
     triangle = new Triangle(x, y, z);
 
