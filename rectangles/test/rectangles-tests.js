181be91a5e5cebcf820a878a21642ad468510534
squeemishly <squeemishly@gmail.com>
2017-08-14 15:40:46 -0600

    creates diagonal test

--- rectangles/test/rectangles-tests.js
+++ rectangles/test/rectangles-tests.js
@@ -1,6 +1,7 @@
 var assert = require('chai').assert;
 var area = require('../area');
 var perimeter = require('../perimeter');
+var diagonal = require('../diagonal')
 
 describe("it can measure a rectangle, specifically: ", function() {
   it("the area", function() {
@@ -11,5 +12,10 @@ describe("it can measure a rectangle, specifically: ", function() {
   it("the perimeter", function() {
     var rectangle = {width: 10, height: 20};
     assert.equal(perimeter(rectangle), 60);
+  })
+
+  it("the diagonal", function() {
+    var rectangle = {width: 7, height: 24};
+    assert.equal(diagonal(rectangle), 25)
   })
 })
