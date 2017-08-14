787b588d34236771d8825164769d445d96eb3802
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-08-14 14:03:04 -0600

    Create test and begin functions for rectangle

---
+++ rectangles.js
@@ -0,0 +1,23 @@
+function area(rectangle){
+  return rectangle.width * rectangle.height;
+};
+
+function perimeter(rectangle){
+  return (rectangle.width * 2) + (rectangle.height * 2);
+};
+
+function diagonal(rectangle){
+  var sum = (Math.pow(rectangle.width, 2) + Math.pow(rectangle.height, 2));
+  return Math.sqrt(sum);
+};
+
+function isSquare(){
+
+};
+
+module.exports = {
+                  area,
+                  perimeter,
+                  diagonal,
+                  isSquare
+                  }
---
+++ test/rectangles-test.js
@@ -0,0 +1,31 @@
+const assert = require('chai').assert;
+const area = require('../rectangles').area
+const perimeter = require('../rectangles').perimeter
+const diagonal = require('../rectangles').diagonal
+
+
+describe("rectangle functions", function() {
+  it("returns the area of the rectangle", function() {
+    var rectangle = {width: 10, height: 20};
+    var expected = 200;
+    var actual = area(rectangle);
+    assert.equal(expected, actual);
+  })
+
+  it("returns the perimeter of the rectangle", function(){
+    var rectangle = {width: 10, height: 20};
+    var expected = 60;
+    var actual = perimeter(rectangle);
+    assert.equal(expected, actual);
+  })
+
+  it("returns the length of the rectangle's diagonal", function(){
+    var rectangle = {width: 10, height: 20};
+    var expected = 22.360679774997898;
+    var actual = diagonal(rectangle);
+    assert.equal(expected, actual);
+  })
+})
+
+
+// isSquare(rectangle) - returns true if the rectangle is a square and false otherwise
