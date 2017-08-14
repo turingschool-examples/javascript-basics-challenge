4600df3a6c012a9b0d8308e7f554e9d543104064
rwilhel <rwilhel@gmail.com>
2017-08-14 17:08:36 -0600

    Completes testing for isTriangle function

--- test/triangles-test.js
+++ test/triangles-test.js
@@ -17,6 +17,10 @@ it('can confirm that a triangle cannot have negative length', function () {
   assert.isFalse(isTriangle(-2, 1, 5));
 });
 
-it('can confirm that a triangle can be found from proper lengths', function () {
+it('can confirm that a triangle can be formed from proper lengths', function () {
   assert.isTrue(isTriangle(6, 8, 10));
+});
+
+it('can confirm that a triangle cannot be formed from improper lengths', function () {
+  assert.isFalse(isTriangle(4, 8, 50));
 });
