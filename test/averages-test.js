0cbd2a7bbc91668493d5050127180ee4ee907c9c
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-14 14:41:54 -0600

    Add test for calculating the number of occurrences of each element

--- test/averages-test.js
+++ test/averages-test.js
@@ -2,6 +2,7 @@ const assert = require('chai').assert;
 const total = require('../lib/averages').total;
 const mean = require('../lib/averages').mean;
 const median = require('../lib/averages').median;
+const element_occurrences = require('../lib/averages').element_occurrences;
 
 describe("Average calculations", () => {
   context("Given a dataset", () => {
@@ -20,6 +21,13 @@ describe("Average calculations", () => {
       var odd_number_of_elements = [3, 1, 2];
       assert.equal(median(even_number_of_elements), 2.5);
       assert.equal(median(odd_number_of_elements), 2);
+    });
+
+    it("calculates the number of occurrences of each element", () => {
+      var data_with_repetition = [5, 5, 2, 5, 2, 7];
+      assert.equal(element_occurrences(data_with_repetition),
+        { 5: 3, 2: 2, 7: 1}
+      );
     });
   });
 });
