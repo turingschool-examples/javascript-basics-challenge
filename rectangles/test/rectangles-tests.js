4ba7a0dcf58f8a9e97e0044a40186ed1c42743bc
squeemishly <squeemishly@gmail.com>
2017-08-14 16:03:25 -0600

    creates test to determine if a rectangle is also a square

--- rectangles/test/rectangles-tests.js
+++ rectangles/test/rectangles-tests.js
@@ -2,6 +2,7 @@ var assert = require('chai').assert;
 var area = require('../area');
 var perimeter = require('../perimeter');
 var diagonal = require('../diagonal')
+var isSquare = require('../is-square')
 
 describe("it can measure a rectangle, specifically: ", function() {
   it("the area", function() {
@@ -17,5 +18,17 @@ describe("it can measure a rectangle, specifically: ", function() {
   it("the diagonal", function() {
     var rectangle = {width: 7, height: 24};
     assert.equal(diagonal(rectangle), 25)
+  })
+})
+
+describe("it can determine the shape", function() {
+  it("square", function() {
+    var rectangle = {width: 7, height: 7};
+    assert.equal(isSquare(rectangle), true);
+  })
+
+  it("rectangle", function() {
+    var rectangle = {width: 10, height: 20};
+    assert.equal(isSquare(rectangle), false);
   })
 })
