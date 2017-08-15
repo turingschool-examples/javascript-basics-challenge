f81c552c6b70f096a15db02680546b9cf0b9928f
Carl Richmond <richmond.carl@outlook.com>
2017-08-14 20:34:17 -0600

    added helper functions and created perimeter and diagonal functions

--- rectangles.js
+++ rectangles.js
@@ -1,12 +1,37 @@
 var pry = require('pryjs');
+// eval(pry.it);
 
-function area(attributes) {
-  eval(pry.it);
+function multiply(input_one, input_two) {
+  return input_one * input_two;
 };
 
+function addition(input_one, input_two) {
+  return input_one + input_two;
+};
 
+function area(attributes) {
+  let width = attributes.width;
+  let height = attributes.height;
 
+  return multiply(width, height);
+};
+
+function perimeter(attributes) {
+  let width = attributes.width;
+  let height = attributes.height;
+
+  return multiply((addition(width, height)), 2);
+};
+
+function diagonal(attributes) {
+  let width = Math.pow(attributes.width, 2);
+  let height = Math.pow(attributes.height, 2);
+
+  return Math.sqrt(addition(width, height));
+};
 
 module.exports = {
-  area
+  area,
+  perimeter,
+  diagonal
 }
--- test/rectangles_test.js
+++ test/rectangles_test.js
@@ -5,9 +5,21 @@ const rectangles = require('../rectangles')
 describe('functions can return different rectangle properties', function() {
 
   it('can return the area of a rectangle', function(){
-    var attributes = {width: 10, height: 20}
+    var attributes = {width: 10, height: 20};
 
     assert.deepEqual(rectangles.area(attributes), (200))
   })
+
+  it('can return the perimeter', function(){
+    var attributes = {width: 10, height: 20};
+
+    assert.deepEqual(rectangles.perimeter(attributes), (60))
+  })
+
+  it('can return the diagonal value', function(){
+    var attributes = {width: 10, height: 20};
+
+    assert.deepEqual(rectangles.diagonal(attributes), (60))
+  });
 
 })
