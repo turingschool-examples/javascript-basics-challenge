b77bf65cb885dee4cf8c9122c946a3930f155958
squeemishly <squeemishly@gmail.com>
2017-08-14 21:19:20 -0600

    classified the functions

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,19 +1,22 @@
-var factorial = function(num) {
-  if (num <= 0) {
-    return "I can only factorialize positive numbers. Please try again.";
-  } else if (typeof num !== 'number'){
-    return "Wrong."
-  } else {
-    return factorialize(num);
+class factorial {
+  response(num) {
+    if (num <= 0) {
+      return "I can only factorialize positive numbers. Please try again.";
+    } else if (typeof num !== 'number'){
+      return "Wrong."
+    } else {
+      return this.factorialize(num);
+    }
+  }
+
+  factorialize(num) {
+    var total = 1;
+    for (var i = 1; i <= num; i++) {
+      total = total * i;
+    }
+    return total;
   }
 }
 
-factorialize = function(num) {
-  var total = 1;
-  for (var i = 1; i <= num; i++) {
-    total = total * i;
-  }
-  return total;
-}
 
 module.exports = factorial;
--- factorial/test/factorial-test.js
+++ factorial/test/factorial-test.js
@@ -3,14 +3,17 @@ var factorial = require('../factorial.js')
 
 describe('find factorials', function() {
   it('can find the factorial of a positive number', function() {
-    assert.equal(factorial(4), 24);
+    var finding = new factorial;
+    assert.equal(finding.response(4), 24);
   })
 
   it('return an error for a number <= 0', function() {
-    assert.equal(factorial(-2), "I can only factorialize positive numbers. Please try again.");
+    var finding = new factorial;
+    assert.equal(finding.response(-2), "I can only factorialize positive numbers. Please try again.");
   })
 
   it('returns an error for non-numbers', function() {
-    assert.equal(factorial("I am not a number"), "Wrong.");
+    var finding = new factorial;
+    assert.equal(finding.response("I am not a number"), "Wrong.");
   })
 })
