998f4c290bd51e2eeed393f6fd719170ddea38a6
rwilhel <rwilhel@gmail.com>
2017-08-14 20:27:14 -0600

    Passes tests for multiplication table

--- test/times-table-test.js
+++ test/times-table-test.js
@@ -4,3 +4,11 @@ const timesTable = require('../times-table.js');
 it('is a function', function () {
   assert.isFunction(timesTable, 'this is a function');
 });
+
+it('can return one row in multiplication table', function () {
+  assert.deepEqual(timesTable(1), '1\t\n');
+});
+
+it('can return five rows in multiplication table', function () {
+  assert.deepEqual(timesTable(5), '1\t2\t3\t4\t5\t\n2\t4\t6\t8\t10\t\n3\t6\t9\t12\t15\t\n4\t8\t12\t16\t20\t\n5\t10\t15\t20\t25\t\n');;
+});
