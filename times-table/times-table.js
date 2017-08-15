5d399114833dcfcf63959846f0eaa9ff0811bed3
squeemishly <squeemishly@gmail.com>
2017-08-14 20:51:17 -0600

    refactored times-table to pull logic to their own functions

--- times-table/times-table.js
+++ times-table/times-table.js
@@ -2,13 +2,20 @@ var pry = require('pryjs')
 
 class timesTable {
   createTable(num) {
-    var output = '';
-    var vals = []
+    var vals = this.createFirstRow(num)
+    return this.fillTable(vals, num);
+  }
+
+  createFirstRow(num) {
+    var arr = []
     for (var i = 1; i <= num; i++) {
-      // output = output + i.toString() + " ";
-      vals.push(i)
-      // eval(pry.it)
+      arr.push(i);
     }
+    return arr;
+  }
+
+  fillTable(vals, num) {
+    var output = '';
     vals.forEach(function(val) {
       if (output !== "") { output = output + "\n" };
       for (var i = 1; i <= num; i++) {
