145448964378600f43917089656baf25d068aef0
squeemishly <squeemishly@gmail.com>
2017-08-14 19:27:15 -0600

    expands test for stringyness of all values to test for response to arrays and objects

--- longest-string/test/longest-string-test.js
+++ longest-string/test/longest-string-test.js
@@ -15,5 +15,7 @@ describe("It can find the longest string", function() {
   it("returns an error if any of the values is not a number", function() {
     var myLongest = new longestString;
     assert.equal(myLongest.findLongest(['Boudi', 'kitty', 5]), "This only works with words.");
+    assert.equal(myLongest.findLongest(['Boudi', 'kitty', ['squee', 'human']]), "This only works with words.");
+    assert.equal(myLongest.findLongest(['Boudi', 'kitty', {name: 'squee', type: 'human'}]), "This only works with words.");
   })
 })
