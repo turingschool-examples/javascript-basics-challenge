e1d2c33320031001b9c8f51eae0636b51d273576
squeemishly <squeemishly@gmail.com>
2017-08-14 19:08:41 -0600

    tests to find the longest string written

---
+++ longest-string/test/longest-string-test.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert;
+var longestString = require('../longest-string')
+
+describe("It can find the longest string", function() {
+  it("when it receives an array of strings", function() {
+    assert.equal(longestString(['horses', 'cats', 'pineapples']), "pineapples");
+  })
+
+  // it("returns null when the array of strings is empty", function() {
+  //   assert.equal(longestString([]), null);
+  // })
+  //
+  // it("returns an error if any of the values is not a number", function() {
+  //   assert.equal(longestString(['Boudi', 'kitty', 5]));
+  // })
+})
