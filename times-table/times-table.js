f4df4019c32692fcae2bfcfde8bf05ee81388398
squeemishly <squeemishly@gmail.com>
2017-08-14 20:41:03 -0600

    times table produces table for all numbers up to the number given

--- times-table/times-table.js
+++ times-table/times-table.js
@@ -5,10 +5,17 @@ class timesTable {
     var output = '';
     var vals = []
     for (var i = 1; i <= num; i++) {
-      output = output + i.toString() + " ";
+      // output = output + i.toString() + " ";
       vals.push(i)
-      eval(pry.it)
+      // eval(pry.it)
     }
+    vals.forEach(function(val) {
+      if (output !== "") { output = output + "\n" };
+      for (var i = 1; i <= num; i++) {
+        output = output + (val * i).toString() + " ";
+      }
+    })
+    return output;
   }
 }
 
