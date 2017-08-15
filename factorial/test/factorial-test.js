bc94c2e61c5750fed3ba337094c49c5b1d471030
squeemishly <squeemishly@gmail.com>
2017-08-14 18:50:13 -0600

    writes the tests

---
+++ factorial/test/factorial-test.js
@@ -0,0 +1,15 @@
+var assert = require('chai').assert;
+
+describe('find factorials', function() {
+  it('can find the factorial of a positive number', function() {
+    assert.equal(factorial(4), 24);
+  })
+
+  it('return an error for a number <= 0', function() {
+    assert.equal(factorial(-2), "I can only factorialize positive numbers. Please try again.");
+  })
+
+  it('returns an error for non-numbers', function() {
+    assert.equal(factorial("I am not a number"), "Wrong.");
+  })
+})
