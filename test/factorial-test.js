7133eb14c00d8daa992e75e38a758cdd9f484e2b
rwilhel <rwilhel@gmail.com>
2017-08-14 18:34:03 -0600

    Passes test for finding factorial of 0 to be 1

--- test/factorial-test.js
+++ test/factorial-test.js
@@ -4,3 +4,7 @@ const factorial = require('../factorial.js');
 it('is a function', function() {
   assert.isFunction(factorial, 'this is a function');
 });
+
+it('can find the factorial of 0 as 1', function() {
+  assert.deepEqual(factorial(0), 1);
+});
