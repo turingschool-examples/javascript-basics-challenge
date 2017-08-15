111f8fade67b045ce9fa478ff33ac4a1d60d8009
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-14 19:07:06 -0600

    Implement making a times table

--- test/times-table-test.js
+++ test/times-table-test.js
@@ -1,10 +1,11 @@
 const assert = require('chai').assert;
+const timesTable = require('../lib/times-table');
 
 describe("Times Table", () => {
   context("Given an integer representing a number of rows", () => {
     it("produces a times table with that number of rows", () => {
       var number_of_rows = 5;
-      var result = "1  2  3  4  5\n2  4  6  8  10\n3  6  9  12 15\n4  8  12 16 20\n5  10 15 20 25";
+      var result = "1  2  3  4  5  \n2  4  6  8  10  \n3  6  9  12  15  \n4  8  12  16  20  \n5  10  15  20  25  \n";
       assert.equal(timesTable(number_of_rows), result);
     });
   });
