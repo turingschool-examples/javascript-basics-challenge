f97d3a45d78e8961f5ef3ce2636f09bfa345b1af
rwilhel <rwilhel@gmail.com>
2017-08-14 20:03:30 -0600

    Passes test for finding the longest string in an array

--- longest-string.js
+++ longest-string.js
@@ -1,5 +1,14 @@
 function longestString(arr) {
+  var strLength = 0;
+  var longest;
 
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > strLength) {
+      var strLength = arr[i].length;
+      longest = arr[i];
+    }
+  }
+  return longest;
 }
 
 module.exports = longestString
--- test/longest-string-test.js
+++ test/longest-string-test.js
@@ -4,3 +4,8 @@ const longestString = require('../longest-string.js');
 it('is a function', function () {
   assert.isFunction(longestString, 'this is a function');
 });
+
+it('can find the longest string in an array', function () {
+  var arr = ["cat", "mouse"];
+  assert.deepEqual(longestString(arr), "mouse");
+});
