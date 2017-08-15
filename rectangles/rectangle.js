44122ee3ad1fe106fb57563415aca37502ea53fb
Sarah Kirk <kirkse710@gmail.com>
2017-08-15 12:04:27 -0600

    adds diagonal function to rectangle

--- rectangles/rectangle.js
+++ rectangles/rectangle.js
@@ -11,4 +11,9 @@ class Rectangle {
   perimeter() {
     return (this.width)*2 + (this.height)*2
   }
+
+  diagonal() {
+    return Math.sqrt(this.width * this.width + this.height * this.height)
+  }
+  
 }
