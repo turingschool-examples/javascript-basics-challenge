0511c4fbfe4964a3bb2ae0de36ee251c0772a0df
Sarah Kirk <kirkse710@gmail.com>
2017-08-15 21:21:26 -0600

    adds isNegative function to isTriangle class

--- triangle.js
+++ triangle.js
@@ -3,10 +3,22 @@ class isTriangle {
     if (this.isZero(side1, side2, side3)) {
       return "A triangle cannot have a missing side!";
     }
+    else if (this.isNegative(side1, side2, side3)) {
+      return "A triangle cannot have negative side!";
+    }
   }
 
   isZero(side1, side2, side3) {
     if (side1 === 0 || side2 === 0, || side3 === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  isNegative(side1, side2, side3) {
+    if (side1 < 0 || side2 < 0 || side3 < 0) {
       return true;
     }
     else {
