91fec55a5cef951bfcce259e0eafca00cd05244c
Alex Koch <alex549us3@gmail.com>
2017-08-26 12:11:37 -0600

    averages mean and sum are done

---
+++ test/averages_test.js
@@ -0,0 +1,23 @@
+const assert = require('chai').assert
+const averages = require('../lib/averages')
+
+describe('Average numbers suite', function() {
+
+  var array = [1, 2, 3, 4, 5]
+
+  it('can add all the numbers in an array', function() {
+    var expected = 15
+    var actual = averages.total(array)
+
+    assert.equal(actual, expected)
+  })
+
+  it('can find the mean of the numbers in the array', function() {
+    var expected = 3
+    var actual = averages.mean(array)
+
+    assert.equal(actual, expected)
+  })
+
+})
+
