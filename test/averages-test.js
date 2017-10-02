56a95c783683940b2aa95dde57ac425a0f069185
Chantal Justamond <cjustamm66@me.com>
2017-10-02 15:38:37 -0600

    median and mode passing :hamster:

--- test/averages-test.js
+++ test/averages-test.js
@@ -11,5 +11,15 @@ describe('stats functions', function () {
    it('returns the average of the array', function () {
         let numbers = [1,2,3,4,5];
         assert.equal(averages.average(numbers), 3)
+   });
+
+   it('return the median of the array', function () {
+       let numbers = [1,2,3,4,5];
+       assert.equal(averages.median(numbers), 3)
+   });
+
+   it('can return the mode of the array', function () {
+       let numbers = [1,2,3,4,5,4,4];
+       assert.equal(averages.mode(numbers), 4)
    })
 });
