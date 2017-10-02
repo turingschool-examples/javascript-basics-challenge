c0bd93f290ea7592aea473bd946fdc91381cbcde
BFunk <bret.e.funk@gmail.com>
2017-10-02 13:24:55 -0600

    added diagonal

--- rectangles.js
+++ rectangles.js
@@ -1,10 +1,17 @@
 function area(shape) {
   return shape.width * shape.height;
-}
+};
 
-function perimeter(rectangle) {
-  return "It works!";
-}
+function perimeter(shape) {
+  return (shape.width + shape.height) * 2;
+};
 
-module.exports = area
-module.exports = perimeter
+function diagonal(shape) {
+  return Math.sqrt(Math.pow(shape.width, 2) + Math.pow(shape.height, 2));
+};
+
+module.exports = {
+  area: area,
+  perimeter: perimeter,
+  diagonal: diagonal
+}
--- tests/rectangles-test.js
+++ tests/rectangles-test.js
@@ -1,12 +1,16 @@
-var assert = require('chai').assert
-var rectangle = require('../rectangles')
+const assert = require('chai').assert
+const rectangle = require('../rectangles')
 
 describe("it tests rectangles", function() {
-  it("returns the rectangle", function() {
-    assert.deepEqual(rectangle[area]({width: 10, height: 20}), 200);
+  it("returns the area", function() {
+    assert.deepEqual(rectangle.area({width: 10, height: 20}), 200);
   });
 
-  it("returns the rectangle", function() {
-    assert.deepEqual(perimeter({width: 10, height: 20}), 60);
+  it("returns the perimeter", function() {
+    assert.deepEqual(rectangle.perimeter({width: 10, height: 20}), 60);
+  });
+
+  it("returns the diagonal", function() {
+    assert.deepEqual(rectangle.diagonal({width: 3, height: 4}), 5);
   });
 });
