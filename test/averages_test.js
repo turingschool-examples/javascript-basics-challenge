1768c3b953e40a0e7c758ba0ca3d5e731e21a7cc
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-02 14:38:11 -0600

    passing mean test and functional mean.js script

--- test/averages_test.js
+++ test/averages_test.js
@@ -1,11 +1,18 @@
 const assert = require('chai').assert;
 const total = require('../averages/total.js');
+const mean = require('../averages/mean.js');
 
 describe("averages functions", function(){
   context("calculating totals", function(){
     it("returns sum values of array", function(){
       array = [25, 57, 10, 8]
       assert.equal(100, total(array))
+    })
+  });
+  context("calculating means", function(){
+    it("returns sum values of array", function(){
+      array = [5, 10, 15, 20, 5]
+      assert.equal(11, mean(array))
     })
   });
 });
