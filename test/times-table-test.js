e84c747afbf9c0e6b13563b88c274ae4acb7c6eb
Chantal Justamond <cjustamm66@me.com>
2017-10-02 17:37:12 -0600

    times tables is passing

---
+++ test/times-table-test.js
@@ -0,0 +1,13 @@
+
+const assert = require('chai').assert;
+const timesTable = require('../times-table');
+
+describe('timesTable', function () {
+    it('returns a table', function () {
+        assert.equal(timesTable(5), '1 2 3 4 5\n' +
+                                    '2 4 6 8 10\n' +
+                                    '3 6 9 12 15\n' +
+                                    '4 8 12 16 20\n' +
+                                    '5 10 15 20 25\n')
+    })
+});
