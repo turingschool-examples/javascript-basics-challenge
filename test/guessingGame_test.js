2ceeaa25cb1a5c5158d9130117bea879ea0d7621
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-02 17:00:33 -0600

    guessingGame.js and guessingGame_test.js created; test fully prepared to match iteration requirements

---
+++ test/guessingGame_test.js
@@ -0,0 +1,43 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const guessingGame = require('../guessingGame');
+
+var game = new GuessingGame(20);
+
+game.isSolved();   // => false
+
+game.guess(5);     // => 'low'
+game.guess(40);    // => 'high'
+game.isSolved();   // => false
+
+game.guess(20);    // => 'correct'
+game.isSolved()    // => true
+
+describe("guessing game unit tests", function(){
+  context("object constructor object test", function(){
+    var game = new GuessingGame(20);
+    it("can be iniated", function(){
+      expect(game).to.exist
+    })
+  });
+  context("object constructor function tests", function(){
+    var game = new GuessingGame(20);
+    it("is not initially solved", function(){
+      assert.equal(false, game.isSolved())
+    }
+    it("low guesses return 'low'", function(){
+      assert.equal('low', game.guess(5))
+    })
+    it("high guesses return 'high'", function(){
+      assert.equal('high', game.guess(25))
+      assert.equal(false, game.isSolved())
+    })
+    it("correct guesses return 'correct'", function(){
+      assert.equal('correct', game.guess(20))
+    })
+    it("can be solved", function(){
+      game.guess(20)
+      assert.equal(true, game.isSolved())
+    })
+  });
+});
