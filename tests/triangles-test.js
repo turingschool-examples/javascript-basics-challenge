d6a6100e92cb1963e1ee6d4ba7e0c52f8e3fedeb
BFunk <bret.e.funk@gmail.com>
2017-10-02 14:05:38 -0600

    working triangle methods and tests

--- tests/triangles-test.js
+++ tests/triangles-test.js
@@ -3,6 +3,15 @@ const triangles = require('../triangles')
 
 describe("it returns traingle authentication", function() {
   it("returns true with valid numbers", function() {
-    assert.deepEqual(triangles.isTriangle(0,0,0), "it works!");
+    assert.deepEqual(triangles.isTriangle(4,4,4), true);
+    assert.deepEqual(triangles.isTriangle(6,8,10), true);
+    assert.deepEqual(triangles.isTriangle(8,6,10), true);
+    assert.deepEqual(triangles.isTriangle(10,8,6), true);
+  });
+
+  it("returns false with valid numbers", function() {
+    assert.deepEqual(triangles.isTriangle(0,0,0), false);
+    assert.deepEqual(triangles.isTriangle(-2,1,5), false);
+    assert.deepEqual(triangles.isTriangle(4,8,50), false);
   });
 });
--- triangles.js
+++ triangles.js
@@ -1,5 +1,5 @@
-function isTriangle(numbers) {
-  return "it works!";
+function isTriangle(num1, num2, num3) {
+  return ((num1 + num2 > num3) && (num1 + num3 > num2) && (num2 + num3 > num1))
 };
 
 module.exports = {
