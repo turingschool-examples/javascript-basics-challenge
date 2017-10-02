00e9bc30978c8f4c708a1710e238ba4eafda9ad0
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-02 14:54:59 -0600

    median tests added and functions written

--- averages.js
+++ averages.js
@@ -1,3 +1,5 @@
+var pry = require('pryjs');
+
 function total(array) {
   var sum = 0
   array.forEach(function(num) {
@@ -6,4 +8,19 @@ function total(array) {
   return sum;
 }
 
-module.exports = {total}
+function mean(array) {
+  return total(array)/array.length;
+}
+
+function median(array) {
+  var sorted = array.sort()
+  if (sorted.length % 2 != 0 ) {
+    var index = (sorted.length/2) - .5
+    return sorted[index];
+  } else {
+    var indexes = new Array((sorted.length/2) - .5, (sorted.length/2) + .5)
+    return mean(indexes);
+  }
+}
+
+module.exports = {total, mean, median}
--- test/averagesTest.js
+++ test/averagesTest.js
@@ -8,4 +8,23 @@ describe('test averages', function () {
       assert.equal(averages.total(array), 3)
     })
   })
+
+  context('testing mean', function() {
+    it('returns the mean', function() {
+      array = new Array(2, 1, 3)
+      assert.equal(averages.mean(array), 2)
+    })
+  })
+
+  context('testing median', function() {
+    it('returns the median', function() {
+      array = new Array(2, 1, 3)
+      assert.equal(averages.median(array), 2)
+    })
+
+    it('returns the median of an even array', function() {
+      array = new Array(1, 2, 2, 4)
+      assert.equal(averages.median(array), 2)
+    })
+  })
 })
