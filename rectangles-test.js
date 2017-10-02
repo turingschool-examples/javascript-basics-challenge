94c03cab18aff6ff68aaa7725a1ed52bce7c17db
MasonHolland <masontholland@gmail.com>
2017-10-02 14:19:01 -0600

    created test for rectanlge

---
+++ rectangles-test.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert
+const area = require('./rectangles')
+const perimeter = require('./rectangles')
+const diagonal = require('./rectangles')
+const isSquare = require('./rectangles')
+describe("rectangle calculations can be made", function() {
+  context("when calling on area", function() {
+
+  })
+
+  context("when calling on permimeter", function() {
+    it("returns the perimeter of a given rectangle", function() {
+
+    })
+  })
+
+  context("when calling on diagonal", function() {
+    it("returns the diagonal of a given rectangle", function() {
+
+    })
+  })
+
+  context("when calling on isSquare", function() {
+    it("returns a true or false response when given a rectangle", function() {
+
+    })
+  })
+})
