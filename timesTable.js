dc6fd7dca322b1fd91f06333a6ef403640cc89b3
BFunk <bret.e.funk@gmail.com>
2017-10-02 17:12:07 -0600

    timesTable works and is outputing, unsure how to test

--- timesTable.js
+++ timesTable.js
@@ -1,6 +1,17 @@
 function timesTable(number) {
-  return "it works!"
+  let output = "";
+  for (let i = 1; i < number + 1; i++) {
+    for (let j = 1; j < number + 1; j++) {
+      let num = i * j;
+      output = output.concat(" " + num.toString());
+    };
+   output = output.concat("\n");
+  };
+  return output;
+  console.log('test');
 };
+
+console.log(timesTable(5));
 
 module.exports = {
   timesTable: timesTable
