7a2b81c5138ed4b9d23cc46ad6089c70228cce87
BFunk <bret.e.funk@gmail.com>
2017-10-02 14:18:49 -0600

    working mean test and function

--- averages.js
+++ averages.js
@@ -4,6 +4,15 @@ function total(numbers) {
   });
 };
 
+function mean(numbers) {
+  let total = numbers.reduce(function(a,b) {
+    return a + b;
+  });
+  return total / numbers.length;
+};
+
 module.exports = {
-  total: total
+  total: total,
+  mean: mean
+
 }
--- tests/averages-test.js
+++ tests/averages-test.js
@@ -5,4 +5,8 @@ describe("checks for average", function() {
   it("returns the total", function() {
     assert.deepEqual(averages.total([1, 2, 3]), 6)
   });
+
+  it("returns the mean", function() {
+    assert.deepEqual(averages.mean([13, 18, 13, 14, 13, 16, 14, 21, 13]), 15)
+  });
 });
