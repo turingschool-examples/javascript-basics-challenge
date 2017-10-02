1bf81a026b68db988ebb8fb1681764606c809d48
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-02 14:19:13 -0600

    fully functioning with new isTriangle conditional structure, passign tests

--- test/triangles_test.js
+++ test/triangles_test.js
@@ -1,13 +1,18 @@
 const assert = require("chai").assert;
 const isTriangle = require("../triangles/isTriangle");
+const pry = require('pryjs');
 
 
 describe("is object a triangle test", function(){
   it("returns false if any of the sides are zero", function(){
-    assert.isFalse(isTriangle(0,0,0));
-    assert.isFalse(isTriangle(1,4,0));
+    assert.equal("false, because a triangle can't have 0 length", isTriangle(1,4,0));
+    assert.equal("false, because a triangle can't have 0 length", isTriangle(0,0,0));
   })
   it("returns false if any of the sides are negative", function(){
-    assert.isFalse(isTriangle(1,3,-2));
+    assert.equal("false, because a triangle can't have a negative dimension", isTriangle(1,3,-2));
+
+  })
+  it("knows an equilateral triangle when it sees one", function(){
+    assert.equal("true, this is an equilateral triangle", isTriangle(4,4,4))
   })
 });
--- triangles/isTriangle.js
+++ triangles/isTriangle.js
@@ -1,25 +1,33 @@
 var pry = require("pryjs");
 
 function isTriangle(a,b,c){
-  var triangleChecks = [];
-  triangleChecks.push(hasZero(a,b,c))
-  triangleChecks.push(hasNegative(a,b,c))
-  if(triangleChecks.includes(false)){
-    return false
+  if (hasZero(a,b,c)){
+    return hasZero(a,b,c);
+  }else if(hasNegative(a,b,c)){
+    return hasNegative(a,b,c);
+  }else if(isEquilateral(a,b,c)){
+    return isEquilateral(a,b,c);
+  }else{
+    return true
   }
-  return true;
 };
 
 function hasNegative(a,b,c){
   if(a < 0 || b < 0 || c < 0){
-    return false
+    return "false, because a triangle can't have a negative dimension"
   }
 };
 
 function hasZero(a,b,c){
   if(a == 0 || b == 0 || c == 0){
-    return false
+    return "false, because a triangle can't have 0 length"
   }
 };
+
+function isEquilateral(a,b,c){
+  if(a == b && b == c){
+    return "true, this is an equilateral triangle";
+  }
+}
 
 module.exports = isTriangle
