c9c75b6fa16bb51baa50beff6ad5e4dbe03771c2
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-02 15:05:59 -0600

    creates a mode script that uses a hash structure to total number of occurrences of a given element, then calculates mode thereafter. Passing tests

---
+++ averages/mode.js
@@ -0,0 +1,20 @@
+var mode = function(arr){
+    var frequencyHash = {};
+    for(var i = 0; i < arr.length; i++){
+        if(frequencyHash[arr[i]] === undefined){
+            frequencyHash[arr[i]] = 0;
+        }
+            frequencyHash[arr[i]] += 1;
+    }
+    var greatestFreq = 0;
+    var mode;
+    for(var prop in frequencyHash){
+        if(frequencyHash[prop] > greatestFreq){
+            greatestFreq = frequencyHash[prop];
+            mode = prop;
+        }
+    }
+    return mode;
+}
+
+module.exports = mode;
--- test/averages_test.js
+++ test/averages_test.js
@@ -2,6 +2,7 @@ const assert = require('chai').assert;
 const total = require('../averages/total.js');
 const mean = require('../averages/mean.js');
 const median = require('../averages/median.js');
+const mode = require('../averages/mode.js');
 
 describe("averages functions", function(){
   context("calculating totals", function(){
@@ -22,4 +23,10 @@ describe("averages functions", function(){
       assert.equal(14, median(array))
     })
   });
+  context("calculating modes", function(){
+    it("returns mode values of array", function(){
+      array = [5, 15, 16, 16, 10, 4, 4, 4, 13, 1];
+      assert.equal(4, mode(array));
+    })
+  })
 });
