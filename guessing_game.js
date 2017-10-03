ca3486368aaafc5bf0531657edc8d8058ba8c638
Yohanan Assefa <nanahoy@gmail.com>
2017-10-03 08:02:12 -0600

    Add code and test for guessing game

---
+++ guessing_game.js
@@ -0,0 +1,19 @@
+function GuessingGame(answer) {
+  this.answer = answer;
+  this.solved = false;
+  this.guess = function(guess) {
+    if(this.answer === guess) {
+      this.solved = true
+      return 'correct';
+    } else if (this.answer < guess) {
+      return 'high';
+    } else {
+      return 'low';
+    }
+  };
+  this.isSolved = function() {
+    return this.solved;
+  }
+}
+
+module.exports = { GuessingGame }
