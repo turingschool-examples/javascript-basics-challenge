e4b44a8891c5a6e488ea998eecfaab2c4c7ffddd
BFunk <bret.e.funk@gmail.com>
2017-10-02 20:33:59 -0600

    added JS string interpolation to see if I could do it

--- timesTable.js
+++ timesTable.js
@@ -3,7 +3,7 @@ function timesTable(number) {
   for (let i = 1; i < number + 1; i++) {
     for (let j = 1; j < number + 1; j++) {
       let num = i * j;
-      output += (" " + num.toString());
+      output += ` ${num.toString()}`;
     };
     output += "\n";
   };
