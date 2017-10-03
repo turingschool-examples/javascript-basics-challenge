860ef76f05effe52d134ca77be20c2889de9b19d
MasonHolland <masontholland@gmail.com>
2017-10-02 20:11:46 -0600

    finish factorial and factorial-test

---
+++ factorial.js
@@ -0,0 +1,9 @@
+function factorial(number) {
+  if (number === 0 || number === 1)
+    return 1;
+  for (var i = number - 1; i >= 1; i--)
+    number *= i;
+  return number;
+}
+
+module.exports = factorial
