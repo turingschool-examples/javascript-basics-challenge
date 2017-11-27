59230644143f6f06c3c24a490178eb50791011cc
samuelssnider <sam_redins@yahoo.com>
2017-11-27 16:09:01 -0700

    factorial working, wasn't working on 1 for some reason

--- factorial.js
+++ factorial.js
@@ -1,19 +1,8 @@
-function theFactorial(input){
-  if(input = 1){
-    return 1;
-  }
-  else{
-    return input * theFactorial(input - 1);
-  }
-}
-
 module.exports = {
   factorial: function factorial(input){
     if(input === 0){
       return 1;
-    }
-    else{
-      return theFactorial(input) 
-    }
+    } 
+    return input * factorial(input - 1);
   }
 }
