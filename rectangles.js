798b23e6b87f480972de5c1fae3bca8da33cc052
jayphodges <jayphodges@gmail.com>
2017-11-27 16:59:10 -0700

    Added rectangle functions

---
+++ rectangles.js
@@ -0,0 +1,16 @@
+function area(width, height) {
+    return width * height;
+}
+
+function perimeter(width, height) {
+    return (width + height) * 2;
+}
+
+function diagional(width, height) {
+    return Math.sqrt(a^2 + b^2);
+}
+
+function isSquare(width, height) {
+    if (width == height);
+}
+
