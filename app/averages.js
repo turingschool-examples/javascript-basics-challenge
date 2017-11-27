2eea25df2018e820e2110599f22b921125c7b7e1
sidewinder2020 <sdwinder1@gmail.com>
2017-11-27 15:13:45 -0700

    Finished averages - 3 methods - 1, mode, as yet unfinished

--- app/averages.js
+++ app/averages.js
@@ -1,3 +1,40 @@
-exports.isTriangle = function(tri1, tri2, tri3) {
+exports.totalAverage = function(array) {
+  var newArray = array.reduce(function(total,start) {
+    return total + start;
+  }, 0);
+  return newArray;
+};
 
+exports.meanAverage = function(array) {
+  var newArray = array.reduce(function(total,start) {
+    return total + start;
+  }, 0);
+  return newArray / array.length;
+};
+
+exports.medianAverage = function(array) {
+  var index1 = (array.length / 2) - 1;
+  var index2 = Math.floor(array.length / 2);
+
+  if (array.length % 2 === 0) {
+    return (array[index1] + array[index1 + 1]) / 2;
+  }
+  else {
+    return array[index2];
+  }
+};
+
+exports.modeAverage = function(array) {
+  var newHash = {}
+
+  for (i = 0; i < array.length; i++) {
+    if (newHash[array[i]]) {
+      newHash[array[i]] = value + 1
+    }
+    else {
+      emptyHash[array[i]] = 1
+    }
+  }
+  return newHash
+  // find the max value of the given properties in the hash
 };
--- test/average_test.js
+++ test/average_test.js
@@ -4,17 +4,18 @@ var averages = require("../app/averages");
 describe("Average function thing", function() {
   it("performs average functions on an array", function() {
 
-    var array = [3, 5, 7, 10, 12, 2]
+    var array = [3, 5, 7, 10, 12, 2, 2];
 
-    var triangleCheckOne = averages.total(-2,1,5);
-    var triangleCheckTwo = averages.mean(0,0,0);
-    var triangleCheckThree = averages.median(4,4,4);
-    var triangleCheckThree = averages.mode(4,4,4);
+    var totalAverage = averages.total(array);
+    var meanAverage = averages.mean(array);
+    var medianAverage = averages.median(array);
+    var modeAverage = averages.mode(array);
 
 
-    expect(triangleCheckOne).to.deep.equal(false);
-    expect(triangleCheckTwo).to.deep.equal(false);
-    expect(triangleCheckThree).to.deep.equal(true);
+    expect(totalAverage).to.deep.equal(41);
+    expect(meanAverage).to.deep.equal(5.85714286);
+    expect(medianAverage).to.deep.equal(10);
+    expect(modeAverage).to.deep.equal();
 
     });
   });
