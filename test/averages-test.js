4b3deeec692a014e4b0d4e7df97ee635bb47e2b4
Brandon Stewart <boveus@gmail.com>
2017-11-27 14:23:09 -0700

    add mean computation

--- test/averages-test.js
+++ test/averages-test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var expect    = require("chai").expect;
 var averagesFunctions = require("../lib/averages");
 describe('averages', function() {
-  describe('area', function() {
+  describe('total', function() {
     it('should return the total of the array', function() {
       var array = [20,20,20,50,50]
 
@@ -10,6 +10,16 @@ describe('averages', function() {
       var total = averagesFunctions.total(array)
 
       assert.deepEqual(expectedTotal, total , "total is computed correctly");
+    });
+  });
+  describe('mean', function() {
+    it('should return the mean of the array', function() {
+      var array = [20,20,20,50,50]
+
+      var expectedMean = 32
+      var mean = averagesFunctions.mean(array)
+
+      assert.deepEqual(expectedMean ,mean , "mean is computed correctly");
     });
   });
 });
