eed75fec1b3fceb9ea3b8e5ab49a7896e64a6d2e
Aaron Hursh <amhursh@gmail.com>
2017-11-27 22:37:40 -0700

    Complete working rectangle perimeter function

--- test/rectangle-test.js
+++ test/rectangle-test.js
@@ -1,11 +1,16 @@
 const assert = require('chai').assert;
-const area = require('../lib/rectangle');
+const functions = require('../lib/rectangle');
+const rectangle = {width: 10, height: 20};
 
 describe("Rectangle", function() {
   context("area", function() {
     it("calculates area of a rectangle", function() {
-      var rectangle = {width: 10, height: 20};
-      assert.equal(area(rectangle), 200);
+      assert.equal(functions.area(rectangle), 200);
+    });
+  });
+  context("perimeter", function() {
+    it("calculates perimeter of given rectangle", function() {
+      assert.equal(functions.perimeter(rectangle), 60);
     });
   });
 });
