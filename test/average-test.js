4922f66ba705bcb9b6db6ae83e965f7f1c71fc3a
Aaron Hursh <amhursh@gmail.com>
2017-11-28 00:05:48 -0700

    Add mean function to averages

--- test/average-test.js
+++ test/average-test.js
@@ -1,11 +1,16 @@
 const assert = require('chai').assert;
 const functions = require('../lib/average');
-const arr = [2, 6, 9, 3, 7, 1, 1];
+const arr = [2, 6, 9, 3, 7, 1, 10, 2];
 
 describe("Averages", function() {
   context("total", function() {
     it("returns the total of all values in an array", function() {
-      assert.equal(functions.total(arr), 29);
-    })
-  })
-})
+      assert.equal(functions.total(arr), 40);
+    });
+  });
+  context("mean", function() {
+    it("returns the average of elements in an array", function() {
+      assert.equal(functions.mean(arr), 5);
+    });
+  });
+});
