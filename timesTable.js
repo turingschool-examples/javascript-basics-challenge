4bcf9b5c2a5626b30626bab449b63334ea36e587
samuelssnider <sam_redins@yahoo.com>
2017-11-27 22:07:36 -0700

    can't figure out how to interpolate the in the regexp

--- timesTable.js
+++ timesTable.js
@@ -1,11 +1,12 @@
 module.exports = {
   timesTable: function timesTable(input){
     returnArr = [];
+    var testVar = input
     for(var i = 1; i <= input; i ++){
       for(var j = 1; j <= input; j ++){
         returnArr.push(i * j);
       }
     }
-    return returnArr.toString();
+    return returnArr.toString().replace(/((?:,[^,]*){2}),/g, '$1\n');
   }
 } 
