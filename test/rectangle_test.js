52c09ecad11151040332c1d0c63683e4278b9ae9
snayrouz <samuel.nayrouz@gmail.com>
2017-11-27 18:13:00 -0700

    Rectangle area passes

--- test/rectangle_test.js
+++ test/rectangle_test.js
@@ -3,10 +3,10 @@ const Rectangle = require('../lib/rectangle');
 
 describe('Rectangle', function() {
   var rectangle = new Rectangle;
-  it('can find the area', () => {
-    var shape = {width: 10, height: 20};
-    assert.equal(rectangle.area(shape), 200);
-  });
+    it('can find the area', () => {
+      var shape = {width: 10, height: 20};
+      assert.equal(rectangle.area(shape), 200);
+    });
 
 
 
