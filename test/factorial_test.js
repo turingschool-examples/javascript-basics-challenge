e014ef34a5251b4a693dc199533f33476c07fcb6
sidewinder2020 <sdwinder1@gmail.com>
2017-11-27 20:56:48 -0700

    factorials done

---
+++ test/factorial_test.js
@@ -0,0 +1,14 @@
+var expect    = require("chai").expect;
+var factorials = require("../app/factorials");
+
+describe("Average function thing", function() {
+  it("performs average functions on an array", function() {
+
+    var num = 7;
+
+    var factThing = factorials.factorial(num);
+
+    expect(totalAverage).to.deep.equal(252);
+
+    });
+  });
