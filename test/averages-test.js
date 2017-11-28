1b19e41b9f59e781c88571066c43b965675ab211
Boots2291 <buteramv@gmail.com>
2017-11-27 17:22:30 -0700

    Add averages

--- test/averages-test.js
+++ test/averages-test.js
@@ -5,7 +5,7 @@ const median = require('../lib/averages').median;
 const mode = require('../lib/averages').mode;
 
 describe("Averages Calculations", function() {
-  var numset = [1, 4, 4, 5, 8, 9, 10]
+  var numset = [1, 4, 4, 5, 8, 9, 11]
 
   it("can calculate the total", function() {
     assert.deepEqual(total(numset), 42)
@@ -15,11 +15,17 @@ describe("Averages Calculations", function() {
     assert.deepEqual(mean(numset), 6)
   })
 
-  it("can calculate the diagonal", function() {
+  it("can calculate the median", function() {
     assert.deepEqual(median(numset), 5)
   })
 
-  it("can determine if square", function() {
-    assert.deepEqual(mode(numset), 4)
+  it("can determine mode", function() {
+    var modeNumset = [1, 1, 2, 2, 4, 5]
+    assert.deepEqual(mode(modeNumset), {"1": 2, "2": 2, "4": 1, "5": 1})
+  })
+
+  it("can determine multiple modes", function() {
+    var modeNumset = [1, 1, 2, 2, 4, 5]
+    assert.deepEqual(mode(modeNumset), {"1": 2, "2": 2, "4": 1, "5": 1})
   })
 })
