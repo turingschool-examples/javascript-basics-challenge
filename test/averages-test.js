a9e5a8b25e167966322384aa4e1356ef270dfa59
Mike Heft <mikeheft@gmail.com>
2017-11-27 17:12:19 -0700

    Averages done

---
+++ test/averages-test.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const pry = require('pryjs')
+const average = require('../lib/averages')
+
+describe('Averages', function(){
+
+  it('can find total of an array', function() {
+    assert.equal(average.total([1,2,3,4]), 10)
+  });
+
+  it('can find mean of array', function() {
+    assert.equal(average.mean([1,2,3,4]),2.5)
+  });
+
+  it('can find median', function() {
+    assert.equal(average.median([1,2,3,4]),2.5)
+  });
+
+  it('can find the mode', function() {
+    assert.deepEqual(average.mode([1,2,2,3]), { '1': 0, '2': 1, '3': 0 })
+  });
+})
