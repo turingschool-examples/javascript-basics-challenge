d8432027ef5a77a386f7314865dddcb8f2ff7c90
Marshall Houston <marshall.houston@gmail.com>
2018-01-22 21:00:28 -0700

    add tests and function for #5 Longest String

---
+++ test/longestString-test.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert
+const longestString = require('../lib/longestString')
+
+describe('longestString(array) functionality', function() {
+  context('with an array of strings as an argument', function() {
+    it('returns the string with the most characters', function () {
+      array = ['hi', 'b', 'beep']
+      assert.equal(longestString.longestString(array), 'beep')
+      array2 = ['burnley', 'socccceeerrrrr', 'manchester']
+      assert.equal(longestString.longestString(array2), 'socccceeerrrrr')
+    })
+  })
+})
