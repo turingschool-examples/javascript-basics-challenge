9ee46fc134332ad0855caa96ff09a8c62f115da8
seoulstice <jungyoungjoon@gmail.com>
2018-03-12 15:47:02 -0600

    Created triangles.js file and isTriangle function

---
+++ triangles.js
@@ -0,0 +1,12 @@
+
+
+function isTriangle(arg1, arg2, arg3) {
+  if(arg1 <= 0  || arg2 <= 0 || arg3 <= 0 ) {
+    return false
+  }
+  // else if()
+}
+
+
+
+module.exports = { isTriangle:isTriangle }
