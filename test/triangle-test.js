679860f60c73603765447d752f715ddf353190ed
seoulstice <jungyoungjoon@gmail.com>
2018-03-12 15:57:01 -0600

    Added assertions to triangle-test and added Pry

--- test/triangle-test.js
+++ test/triangle-test.js
@@ -1,8 +1,12 @@
 var assert = require('chai').assert;
 var isTriangle = require('../triangles').isTriangle
+var pry = require('pryjs')
 
 describe('Triangle functions', function() {
   it('can determine if a shape is a triangle', function() {
     assert.equal(isTriangle(0,0,0), false)
+    assert.equal(isTriangle(4,9,2), false)
+    assert.equal(isTriangle(3,4,5), true)
+
   })
 })
