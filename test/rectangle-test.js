645d0c51ecc44c9f1a33015c423e2082aeaa65a6
leepuppychow <leepuppychow@gmail.com>
2018-03-12 13:36:34 -0600

    Rectangle tests passing

--- test/rectangle-test.js
+++ test/rectangle-test.js
@@ -7,6 +7,24 @@ describe("Rectangle functions", () => {
     assert.equal(r.area(), 200)
   })
 
-  
+  it("can find the perimeter of a rectangle", () => {
+    let r = new Rectangle({width:10, height:20})
+    assert.equal(r.perimeter(), 60)
+  })
+
+  it("can find the length of the rectangle's diagonal", () => {
+    let r = new Rectangle({width:10, height:20})
+    assert.equal(r.diagonal(), 22.36)
+  })
+
+  it("can determine if the rectangle is a square", () => {
+    let r = new Rectangle({width:10, height:20})
+    assert.equal(r.isSquare(), false)
+
+    let s = new Rectangle({width:10, height:10})
+    assert.equal(s.isSquare(), true)
+  })
+
+
 
 })
