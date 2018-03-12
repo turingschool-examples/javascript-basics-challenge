ab6ce949fd866ec0ebba016e5b86269039d5a77e
seoulstice <jungyoungjoon@gmail.com>
2018-03-12 15:57:16 -0600

    Completed isTriangle function using Triangle Inequality Theorem

--- triangles.js
+++ triangles.js
@@ -4,7 +4,12 @@ function isTriangle(arg1, arg2, arg3) {
   if(arg1 <= 0  || arg2 <= 0 || arg3 <= 0 ) {
     return false
   }
-  // else if()
+  if(arg1 + arg2 < arg3 || arg2 + arg3 < arg1 || arg1 + arg3 < arg2) {
+    return false
+  }
+  else {
+    return true
+  }
 }
 
 
