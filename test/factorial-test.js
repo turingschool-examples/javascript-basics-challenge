3cfc510b366bc81c58894ecef08dfac51425cf21
leepuppychow <leepuppychow@gmail.com>
2018-03-12 15:13:08 -0600

    Factorial done

---
+++ test/factorial-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const Factorial = require('../lib/factorial.js')
+
+describe("Factorial Test", () => {
+  it("can find the factorial of a number", () => {
+    let x = new Factorial(5)
+    assert.equal(x.factorial(5), 120)
+
+  })
+})
