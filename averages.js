d573b74004752b63f980a6b035243b26955d4bd7
seoulstice <jungyoungjoon@gmail.com>
2018-03-12 16:57:20 -0600

    Created getMedian function, passing test.

--- averages.js
+++ averages.js
@@ -4,18 +4,30 @@ function getSum(total, num) {
 }
 
 function getTotal(array) {
-  return array.reduce(getSum)
+  return array.reduce(getSum);
 }
 
-// mean from array
-
+// mean from an array
 
 function calculateAverage(array) {
   return (array.reduce(getSum) / array.length);
 }
 
+// median from an array
+
+function getMedian(array) {
+  var median = 0, size = array.length
+  array.sort();
+  if(array.length % 2 === 0) {
+    median = (array[(size / 2) - 1] + array[size / 2]) / 2;
+  }
+  else {
+    median = array[(size - 1) / 2];
+  }
+  return median
+}
 
 
 
 
-module.exports = { calculateAverage:calculateAverage, getTotal:getTotal }
+module.exports = { calculateAverage:calculateAverage, getTotal:getTotal, getMedian:getMedian }
