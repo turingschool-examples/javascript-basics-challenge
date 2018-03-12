b3a832405dd879de4b523b7a4a937118ba2805dd
seoulstice <jungyoungjoon@gmail.com>
2018-03-12 14:58:44 -0600

    Created test file for rectangle.js
    Tests for area/perimeter/diagonal completed

---
+++ test/rectangle-test.js
@@ -0,0 +1,18 @@
+var assert = require('chai').assert;
+var area = require('../rectangle').area
+var perimeter = require('../rectangle').perimeter
+var diagonal = require('../rectangle').diagonal
+
+describe('Rectangle functions', function() {
+  it('can calculate the area', function() {
+    assert.equal(area({width: 10, height: 20}), 200)
+  })
+
+  it('can calculate the perimeter', function() {
+    assert.equal(perimeter({width: 10, height: 20}), 60)
+  })
+
+  it('can calculate the diagonal length', function() {
+    assert.equal(diagonal({width: 10, height: 20}), Math.sqrt(500))
+  })
+})
