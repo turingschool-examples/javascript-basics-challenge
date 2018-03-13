c821abe0dc35a2fe269c3e2ab714d585fd987f69
Tyjoo26 <Tyjoo21@gmail.com>
2018-03-12 19:49:05 -0600

    factorial methodbuilt and test

--- average.js
+++ average.js
@@ -1,4 +1,4 @@
-
+pry = require('pryjs')
 
 var array = [ 1 ,3 ,4 ,5, 6]
 
@@ -10,6 +10,51 @@ function total(data) {
   return total
 }
 
+function mean(data){
+  var sum = 0 ;
+  for (var i = 0; i < data.length ; i++){
+    sum += data[i];
+  }
+  return (sum / data.length)
+}
+
+//////////
+
+function reOrganize(array, index, newIndex) {
+  var temp = array[index];
+  array[index] = array[newIndex];
+  array[newIndex] = temp;
+}
+
+function bubbleSort(array){
+  for( var index = 0 ; index < array.length; index++){
+    for(var newIndex = 1; newIndex < array.length; newIndex++){
+      if(array[newIndex-1] > array[newIndex]){
+        reOrganize(array, newIndex-1, newIndex);
+      }
+    }
+  }
+  return array
+}
 
 
-module.exports = {total : total}
+function median(data){
+  let median = 0;
+  let length = data.length;
+    data.sort();
+    if (length % 2 === 0) {
+      median = (data[length / 2 - 1] + data[length / 2]) / 2;
+    } else {
+      median = data[(length - 1) / 2];
+    }
+  return median;
+}
+
+function mode(data) {
+  
+}
+
+
+
+
+module.exports = {total : total, mean: mean, median: median, mode: mode}
--- test/averages-test.js
+++ test/averages-test.js
@@ -2,7 +2,9 @@ var chai = require('chai');
 var assert = require('assert');
 var pry = require('pryjs');
 var total = require('../average').total
-
+var mean = require('../average').mean
+var median = require('../average').median
+var mode = require('../average').mode
 
 
 
@@ -11,5 +13,30 @@ describe("Numbers", function(){
     var array = [ 1,3,4,5,6]
 
     assert.equal(total(array), 19)
+  })
+
+
+  it("will return mean of all array values", function(){
+    var array = [ 1,3,4,5,6]
+
+    assert.equal(mean(array), 3.8)
+  })
+
+  it("will return median of all array values in array of odd length", function(){
+    var array = [ 1,3,4,5,6]
+
+    assert.equal(median(array), 4)
+  })
+
+  it("will return median of all array values in array of even length", function(){
+    var array = [ 1,3,4,5,6, 1]
+
+    assert.equal(median(array), 3.5)
+  })
+
+  it("will return mode of array", function(){
+    var array = [ 1, 2, 3, 2, 4, 5, 6, 7, 2]
+
+    assert.equal(mode(array), 2)
   })
 })
