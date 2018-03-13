e2cf4bbcfef5a6a8e9b919bdfa71852847dc3050
BLaurenB <blbillington1@gmail.com>
2018-03-12 20:14:29 -0600

    total, mean, and mode working; mediam was but I broke something

--- averages.js
+++ averages.js
@@ -1,4 +1,4 @@
-let someArray = [1, 2, 3, 4, 5, 6]
+let someArray = [1, 2, 2, 2, 3, 3, 4, 5, 6]
 
 function total(someArray) {
   let thisTotal = someArray.reduce(function (total, num) { 
@@ -13,20 +13,36 @@ function mean(someArray) { return total(someArray) / someArray.length }
 
 function median(someArray) {
   //the middle number by count, or the avg of the two centermost numbers
+  if (someArray.length % 2 === 0) {
+    index1 = (someArray.length / 2)
+    index2 = index1 + 1
+    return (someArray[index1] + someArray[index2]) / 2
+  } else {
+    let index = (someArray.length / 2).floor
+    return someArray[index]
+  }
 }
 
 
 function mode(someArray) {
   //the value which occurs most often in an array. a set with mutliple modes is multimodal
+  let kvPairs = someArray.reduce(function(obj, num){
+    // console.log(obj, num)
+    if (!obj[num]) {
+      obj[num] = 0
+    } 
+    obj[num]++
+    return obj
+  }, {})
+  return kvPairs
 }
-
-
-
 
 
 
 console.log(total(someArray))
 console.log(mean(someArray))
+console.log(median(someArray))
+console.log(mode(someArray))
 // Write a function for each of the following. Each function will take an Array as the one argument..
 
 // total - returns the total of all the values in the array
