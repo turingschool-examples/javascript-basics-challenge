79a1c406c139131b206bb11ee79e76ce97f8a4e5
Tyjoo26 <Tyjoo21@gmail.com>
2018-03-14 08:05:07 -0600

    refactored longest function, test passes

--- longest.js
+++ longest.js
@@ -6,14 +6,8 @@ var animals = ["manbearpig", "manimal", "donkey", "tiger"]
 
 function longest(data){
   var tempLongest = undefined
-  if(data.length === 0) {
-    return undefined;
-  }
   data.forEach(string => {
-    if (tempLongest === undefined){
-      tempLongest = string;
-    }
-    else if (string.length > tempLongest.length){
+    if (tempLongest === undefined || string.length > tempLongest.length){
       tempLongest = string;
     }
   })
